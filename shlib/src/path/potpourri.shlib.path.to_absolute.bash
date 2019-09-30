a653980d85a41ad205062fc66dc6502582600971
Potpourri <nikita_belyakov@outlook.com>
2019-10-01 00:33:30 +0300

    fix(shlib): Fix bugs in shlib.path.to_absolute
    - check relativeBase is absolute path, else error
    - fix two line output when path already absolute
    - support following slash in relativeBase
    - rebuild all dependent packages

--- shlib/src/path/potpourri.shlib.path.to_absolute.bash
+++ shlib/src/path/potpourri.shlib.path.to_absolute.bash
@@ -4,8 +4,16 @@ shlib.path.to_absolute() {
 	local -r path=$1
 	local -r relativeBase=${2:-$PWD}
 
+	if ! shlib.path.is_absolute "$relativeBase"; then
+		return 1
+	fi
 	if shlib.path.is_absolute "$path"; then
 		echo "$path"
+		return 0
 	fi
-	echo "$relativeBase/$path"
+	if [[ ${relativeBase: -1} == / ]]; then
+		echo "$relativeBase$path"
+	else
+		echo "$relativeBase/$path"
+	fi
 }
